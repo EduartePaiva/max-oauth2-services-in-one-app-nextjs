51c819d0e7d3efb7016cfd981f1c58b0ba090774
Eduarte <74308809+EduartePaiva@users.noreply.github.com>
2024-11-30 11:04:44 -0300

    configured env variables

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
+import { env as envClient } from "@/env/client";
+import { env as envServer } from "@/env/server";
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const envs = [envClient, envServer];
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
 };
 
 export default nextConfig;
---
+++ src/db/index.ts
@@ -0,0 +1,8 @@
+// Make sure to install the 'postgres' package
+import { env } from "@/env/server";
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+
+const queryClient = postgres(env.DATABASE_URL);
+const db = drizzle({ client: queryClient });
+export default db;
