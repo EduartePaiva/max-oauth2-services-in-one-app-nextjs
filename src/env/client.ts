4a0659832f31718de6e6ecbab8867cbfd5c87af7
Eduarte <74308809+EduartePaiva@users.noreply.github.com>
2024-11-30 11:46:09 -0300

    drizzle with postgress docker setted up

--- src/env/client.ts
+++ src/env/client.ts
@@ -1,6 +1,11 @@
 import { createEnv } from "@t3-oss/env-nextjs";
+import { ZodError } from "zod";
 
 export const env = createEnv({
     client: {},
     runtimeEnv: {},
+    onValidationError: (error: ZodError) => {
+        console.error("❌ Invalid environment variables:", error.flatten().fieldErrors);
+        process.exit(1);
+    },
 });
--- src/env/server.ts
+++ src/env/server.ts
@@ -1,17 +1,21 @@
 import { createEnv } from "@t3-oss/env-nextjs";
-import { z } from "zod";
+import { z, ZodError } from "zod";
 
 export const env = createEnv({
     server: {
         NODE_ENV: z.enum(["development", "production"]),
         DATABASE_URL: z.string().url(),
+        DB_HOST: z.string(),
+        DB_USER: z.string(),
+        DB_PASSWORD: z.string(),
+        DB_NAME: z.string(),
+        DB_PORT: z.coerce.number(),
     },
-    // If you're using Next.js < 13.4.4, you'll need to specify the runtimeEnv manually
-    // runtimeEnv: {
-    //   DATABASE_URL: process.env.DATABASE_URL,
-    //   OPEN_AI_API_KEY: process.env.OPEN_AI_API_KEY,
-    // },
-    // For Next.js >= 13.4.4, you can just reference process.env:
+    onValidationError: (error: ZodError) => {
+        console.error("❌ Invalid environment variables:", error.flatten().fieldErrors);
+        process.exit(1);
+    },
     emptyStringAsUndefined: true,
+    // eslint-disable-next-line n/no-process-env
     experimental__runtimeEnv: process.env,
 });
