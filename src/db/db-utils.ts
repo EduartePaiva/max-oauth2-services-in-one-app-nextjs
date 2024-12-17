aa02c6d651e40fb71af6cb87eef12a1964435bba
Eduarte <74308809+EduartePaiva@users.noreply.github.com>
2024-12-17 16:56:28 -0300

    Feat: updated the logic so that whenever the avatarUrl changes in the provider it automatically update in the database

--- src/db/db-utils.ts
+++ src/db/db-utils.ts
@@ -1,19 +1,28 @@
+import { eq } from "drizzle-orm";
+
+import db from ".";
 import { createUser } from "./db-insert";
 import { getUserFromProviderNameAndId } from "./db-queries";
-import { User } from "./schema";
+import { User, users } from "./schema";
 import { InsertUser } from "./schema/users";
 
 /**
  * @throws this function does requests and can throw
  */
-export async function getOrCreateNewUserAndReturn(
-    userData: InsertUser
-): Promise<User> {
+export async function getOrCreateNewUserAndReturn(userData: InsertUser): Promise<User> {
     const dbUser = await getUserFromProviderNameAndId(
         userData.providerUserId,
         userData.providerName
     );
     if (dbUser !== null) {
+        if (dbUser.avatarUrl !== userData.avatarUrl) {
+            // update the user avatar url with the new avatar url
+            await db
+                .update(users)
+                .set({ avatarUrl: userData.avatarUrl })
+                .where(eq(users.id, dbUser.id));
+        }
+
         return dbUser;
     }
     // TODO: Update the avatar_url if it differ
